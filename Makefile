4a96d263ea6aaf5e390639864b596a59048096d9
Dmitry Kulagin <dkulagin@diagnocat.com>
2020-11-12 18:38:18 +0300

    added jpeg2bmp converter

--- Makefile
+++ Makefile
@@ -2,4 +2,8 @@ build-png2bmp:
 	mkdir -p bin
 	go build -o bin/png2bmp cmd/png2bmp/main.go
 
-build: build-png2bmp
+build-jpeg2bmp:
+	mkdir -p bin
+	go build -o bin/jpeg2bmp cmd/jpeg2bmp/main.go
+
+build: build-png2bmp build-jpeg2bmp
---
+++ cmd/jpeg2bmp/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"image/png"
+	"os"
+
+	"github.com/pkg/errors"
+	"golang.org/x/image/bmp"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("jpeg2bmp input.jpeg output.bmp")
+		return
+	}
+
+	inFn := os.Args[1]
+	outFn := inFn + ".bmp"
+
+	if len(os.Args) >= 3 {
+		outFn = os.Args[2]
+	}
+
+	inF, err := os.Open(inFn)
+	if err != nil {
+		fmt.Println(errors.Wrapf(err, "cannot open input file: %s", inFn))
+		os.Exit(1)
+	}
+
+	img, err := png.Decode(inF)
+	if err != nil {
+		fmt.Println(errors.Wrapf(err, "cannot decode input file: %s", inFn))
+		os.Exit(1)
+	}
+
+	outF, err := os.Create(outFn)
+	if err != nil {
+		fmt.Println(errors.Wrapf(err, "cannot create output file: %s", outFn))
+		os.Exit(1)
+	}
+
+	if err = bmp.Encode(outF, img); err != nil {
+		fmt.Println(errors.Wrap(err, "cannot encode bmp"))
+		os.Exit(1)
+	}
+
+	fmt.Println("done")
+}
